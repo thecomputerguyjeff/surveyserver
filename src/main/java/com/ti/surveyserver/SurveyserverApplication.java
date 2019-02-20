b87d03285aee239fd72a18a14da0d799905e9353
Jeff Mayer <TheComputerGuy.Jeff@gmail.com>
2019-02-19 18:44:48 -0600

    first commit

---
+++ src/main/java/com/ti/surveyserver/SurveyserverApplication.java
@@ -0,0 +1,13 @@
+package com.ti.surveyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyserverApplication.class, args);
+	}
+
+}
