f77ccd2e3e49ac642a94e98cef61671c89c3188d
Jeff Mayer <TheComputerGuy.Jeff@gmail.com>
2019-02-25 19:25:06 -0600

    mongo config

--- src/main/java/com/ti/surveyserver/config/ApplicationConfig.java
+++ src/main/java/com/ti/surveyserver/config/ApplicationConfig.java
@@ -1,9 +1,12 @@
 package com.ti.surveyserver.config;
 
 import com.mongodb.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.util.List;
@@ -18,7 +21,14 @@ public class ApplicationConfig extends AbstractMongoConfiguration {
     protected String getDatabaseName() {
         return "someDB";
     }
+
+
     @Override
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoDbFactory());
+    }
+
     public MongoClient mongoClient() {
         MongoCredential credential = MongoCredential.createCredential("CAIH330", getDatabaseName(), "h8dZHqG7bEqXragz".toCharArray());
         ServerAddress serverAddress = new ServerAddress("mongodb+srv://caih330-jn5ns.mongodb.net");
@@ -30,5 +40,12 @@ public class ApplicationConfig extends AbstractMongoConfiguration {
 //                .writeConcern(WriteConcern.MAJORITY)
 //                .build();
         return new MongoClient(serverAddress, credential, null);
+    }
+
+    @Override
+    @Bean
+    public SimpleMongoDbFactory mongoDbFactory() {
+        return new SimpleMongoDbFactory(
+                mongoClient(), getDatabaseName());
     }
 }
