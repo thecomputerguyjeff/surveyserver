782c049c136449fb044f634b367640c0f1ec23be
aginsparg <alginsparg@gmail.com>
2019-02-26 17:53:53 -0600

    before merging malka's

--- src/main/java/com/ti/surveyserver/repository/SurveyShellRepository.java
+++ src/main/java/com/ti/surveyserver/repository/SurveyShellRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.Repository;
 
 import java.math.BigInteger;
 
-public interface SurveyShellRepository extends Repository<SurveyShell, Long> {
+public interface SurveyShellRepository extends Repository<SurveyShell, BigInteger> {
     SurveyShell findOneById(BigInteger id);
 //    SurveyShell save(SurveyShell customer);
 
